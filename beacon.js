3e12f2b8f40b7a8fbce90327fc807102afddfd83
Alexander Rozhkov <alexander.rozhkov@gmail.com>
2013-04-15 01:14:20 +0300

    Update beacon.js

--- beacon.js
+++ beacon.js
@@ -1,2 +1,44 @@
 /* beacon.js
 */
+var http = require('http');
+var url = require('url');
+
+
+function processRaw(obj) {
+  process.nextTick(function() {
+    console.log('Raw: ' + obj);
+  });
+};
+
+
+function processStat(obj) {
+  process.nextTick(function() {
+    console.log('Stat: ' + obj );
+  });
+};
+
+function beacon (url) {
+
+  process.nextTick(function(){
+
+    console.log('Beacon: ' + url);
+
+    new processRaw(url);
+    new processStat(url);
+
+  });
+};
+
+
+
+http.createServer(function (req, res) {
+
+  new beacon(req.url);
+
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('');
+  console.log('Done ' + req.url);
+
+}).listen(8888, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:8888/');
